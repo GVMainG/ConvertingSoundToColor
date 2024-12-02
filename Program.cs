39615daff5b586e295687d53295bf817cf1b25a2
Georgiy Voronko <GVmainG@yandex.ru>
2024-12-02 15:12:42 +0300

    Добавьте файлы проекта.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace ConvertingSoundToColor
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+        }
+    }
+}
